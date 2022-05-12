b849a323b29bf35f3e9c9783f9f97fb5f331034a
GitNguyenTanNghia <19520774@gm.uit.edu.vn>
2022-05-12 10:38:36 +0700

    Add 3 models + remove ";" in RenderBody layout

---
+++ SE104-QuanLyKhachSan/Models/LoaiKhachHang.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SE104_QuanLyKhachSan.Models
+{
+    public class LoaiKhachHang
+    {
+        private int maLoaiKhachHang;
+        public int MaLoaiKhachHang
+        {
+            get { return maLoaiKhachHang; }
+            set { maLoaiKhachHang = value; }
+        }
+
+        private string tenLoaiKhachHang;
+        public string TenLoaiKhachHang
+        {
+            get { return tenLoaiKhachHang; }
+            set { tenLoaiKhachHang = value; }
+        }
+
+        private byte daXoa;
+        public byte DaXoa
+        {
+            get { return daXoa; }
+            set { daXoa = value; }
+        }
+    }
+}
